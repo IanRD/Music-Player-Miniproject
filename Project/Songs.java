7393f8827b7cdaabc750672243c5be712025c317
Ian Dawson <591290@ad.ccsd.net>
2016-09-09 09:46:09 -0700

    May 2016

---
+++ Project/Songs.java
@@ -0,0 +1,66 @@
+ 
+/**
+ * Song list and sorting
+ * 
+ * @Ian Dawson
+ * @v.1.0
+ *
+ */
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.lang.String;
+import java.util.*;
+public class Songs implements Song
+{
+   /* void color
+    g.setColor(Color.red);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont); */
+
+void songEighties extends Songs
+{
+
+    g.setColor(Color.blue);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont); 
+}
+void songNineties extends Songs
+{
+
+    g.setColor(Color.green);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont);
+}
+void songAlt extends Songs
+{
+
+    g.setColor(Color.pink);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont); 
+}
+void songRap extends Songs
+{
+
+    g.setColor(Color.purple);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont); 
+}
+void songRNB extends Songs
+{
+
+    g.setColor(Color.yellow);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont); 
+}
+}
+
+/*abstract class songMasterList extends Songs
+{
+
+    g.setColor(Color.maroon);
+    Font songFont = new Font ("Bold", Font.BOLD, 26);
+    setFont(songFont); 
+}*/
---
+++ Project/Songserino.java
@@ -0,0 +1,28 @@
+
+/**
+ * Abstract class Songserino - write a description of the class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Songserino
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    //Construct the song
+    //this one component
+    //next
+    //nexterino
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
